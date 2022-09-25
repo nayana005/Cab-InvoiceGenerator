034e5b6cc8c4cce48f5f91c0d7fccf84d17bbe9e
nayana005 <nayanasuresh1005@gmail.com>
2022-09-26 01:26:11 +0530

    UserId Returns Invoice

--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -1,3 +1,4 @@
+
 package com.bridgelabz;
 
 import org.junit.Assert;
@@ -33,6 +34,29 @@ public class CabInvoiceGeneratorTest {
 	        Ride[] rides = new Ride[]{ride1,ride2,ride3};
 	        double totalFare = CabInvoiceGenerator.calculateFare(rides);
 	        Assert.assertEquals(150.0, totalFare);
+	    }
+
+	  @Test
+	  public void givenUserIdShouldReturnInvoice(){
+	        Ride ride1 = new Ride(4,10);
+	        Ride ride2 = new Ride(5,10);
+	        Ride ride3 = new Ride(3,10);
+	        Ride[] rides = new Ride[]{ride1,ride2,ride3};
+	        Customer customer = new Customer(1);
+	        customer.rideList = rides;
+//	        customer.rideList.add(ride1);
+//	        customer.rideList.add(ride2);
+//	        customer.rideList.add(ride3);
+	        CabInvoiceGenerator.customerList.add(customer);
+	        Ride ride4 = new Ride(3,15);
+	        Ride[] rides1 = new Ride[]{ride4};
+	        Customer customer2 = new Customer(2);
+	        customer2.rideList = rides1;
+//	        customer2.rideList.add(ride4);
+	        CabInvoiceGenerator.customerList.add(customer2);
+	       Invoice actualInvoice = CabInvoiceGenerator.generateInvoice(1);
+	        Invoice expectedInvoice = new Invoice(3,150.0,50.0);
+	        Assert.assertEquals(expectedInvoice,actualInvoice);
 	    }
 
 
